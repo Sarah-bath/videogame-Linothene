2310b2522d755dab7217859fcb49b001767dc708
Estebel <63934532+Estebel@users.noreply.github.com>
2020-04-19 02:54:02 +0200

    Bon, ça à l'air simple en fait

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-let mySprite = sprites.create(img`
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.baDing.play()
+    mySprite2.destroy(effects.trail, 100)
+    mySprite.say("C bon sa mèr", 2000)
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -17,3 +24,23 @@ let mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
 controller.moveSprite(mySprite)
+mySprite2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 7 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+. . . . . . 2 2 . . . . . . . . 
+. . . . . . 2 2 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Food)
+mySprite2.setPosition(15, 30)
+mySprite.setFlag(SpriteFlag.BounceOnWall, true)
